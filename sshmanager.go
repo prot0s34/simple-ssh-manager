085f263ecc99d43f386105ca8060678e5d0d5d06
Nikita Maslakov <79843027+prot0s34@users.noreply.github.com>
2023-10-14 00:42:12 +0300

    use single inventory
    use single inventory instead two separate inventory files

--- sshmanager.go
+++ sshmanager.go
@@ -7,24 +7,21 @@ import (
 	"os"
 	"os/exec"
 	"os/user"
-	"strconv"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
-// Host represents a host entry in the inventory.
 type Host struct {
 	Name     string `json:"name"`
 	Hostname string `json:"hostname"`
-	Username string `json:"username,omitempty"` // Added "omitempty" to make it optional
+	Username string `json:"username,omitempty"`
 	Password string `json:"password"`
 }
 
-// Inventory represents the list of hosts.
 type Inventory struct {
-	InventoryName string `json:"inventory_name"`
-	Hosts         []Host `json:"hosts"`
+	InventoryGroup1 []Host `json:"inventory_group1"`
+	InventoryGroup2 []Host `json:"inventory_group2"`
 }
 
 func main() {
@@ -34,15 +31,7 @@ func main() {
 		}
 	}()
 
-	// Load the first inventory from the environment variable or default location
-	inventory1, err := loadInventoryByIndex(1)
-	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
-	}
-
-	// Load the second inventory (index 2)
-	inventory2, err := loadInventoryByIndex(2)
+	inventory, err := loadInventory()
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
@@ -50,25 +39,23 @@ func main() {
 
 	app := tview.NewApplication()
 
-	// Create a list of hosts for the TUI
-	listHostsGroupFirst := createHostList(app, inventory1, inventory1.InventoryName)
-	listHostsGroupSecond := createHostList(app, inventory2, inventory2.InventoryName)
+	listHostsGroupFirst := createHostList(app, inventory.InventoryGroup1, "inventory_group1")
+	listHostsGroupSecond := createHostList(app, inventory.InventoryGroup2, "inventory_group2")
 
 	flex := tview.NewFlex().
 		AddItem(listHostsGroupFirst, 0, 1, true).
 		AddItem(listHostsGroupSecond, 0, 1, true)
 
-	// Define the function to connect to the selected host
-	setHostListSelectedFunc(listHostsGroupFirst, inventory1, app)
-	setHostListSelectedFunc(listHostsGroupSecond, inventory2, app)
+	setHostListSelectedFunc(listHostsGroupFirst, inventory.InventoryGroup1, app)
+	setHostListSelectedFunc(listHostsGroupSecond, inventory.InventoryGroup2, app)
 
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyLeft {
-			// Switch the focus to the next panel.
+
 			app.SetFocus(listHostsGroupFirst)
 		}
 		if event.Key() == tcell.KeyRight {
-			// Switch the focus to the next panel.
+
 			app.SetFocus(listHostsGroupSecond)
 		}
 		return event
@@ -81,8 +68,17 @@ func main() {
 
 }
 
-// loadInventory loads the inventory from the specified JSON file.
-func loadInventory(inventoryPath string) (*Inventory, error) {
+func loadInventory() (*Inventory, error) {
+	inventoryPath := os.Getenv("SSHMANAGER_INVENTORY")
+
+	if inventoryPath == "" {
+		usr, err := user.Current()
+		if err != nil {
+			return nil, err
+		}
+		inventoryPath = usr.HomeDir + "/inventory.json"
+	}
+
 	file, err := ioutil.ReadFile(inventoryPath)
 	if err != nil {
 		return nil, err
@@ -93,27 +89,30 @@ func loadInventory(inventoryPath string) (*Inventory, error) {
 		return nil, err
 	}
 
-	// Set the default username to the current OS username if not defined
 	currentUser, _ := user.Current()
-	for i := range inventory.Hosts {
-		if inventory.Hosts[i].Username == "" {
-			inventory.Hosts[i].Username = currentUser.Username
+	for i := range inventory.InventoryGroup1 {
+		if inventory.InventoryGroup1[i].Username == "" {
+			inventory.InventoryGroup1[i].Username = currentUser.Username
+		}
+	}
+	for i := range inventory.InventoryGroup2 {
+		if inventory.InventoryGroup2[i].Username == "" {
+			inventory.InventoryGroup2[i].Username = currentUser.Username
 		}
 	}
 
 	return &inventory, nil
 }
 
-func createHostList(app *tview.Application, inventory *Inventory, inventoryName string) *tview.List {
+func createHostList(app *tview.Application, hosts []Host, inventoryName string) *tview.List {
 	list := tview.NewList()
 
-	for _, host := range inventory.Hosts {
+	for _, host := range hosts {
 		list.AddItem("name:"+host.Name, "user:"+host.Username+" / hostname:"+host.Hostname, 0, nil).SetBorder(true)
 		list.SetTitle("[black:darkcyan]" + inventoryName + "[white:-]").SetTitleAlign(tview.AlignLeft)
 		list.Box.SetBorderAttributes(tcell.RuneBoard)
 	}
 
-	// Add the custom TextView as the last item in the list
 	list.AddItem("", "'Q' to Quit, < or > to change host list, 'Enter' to connect", 'q', func() {
 		app.Stop()
 	})
@@ -121,19 +120,17 @@ func createHostList(app *tview.Application, inventory *Inventory, inventoryName 
 	return list
 }
 
-func setHostListSelectedFunc(list *tview.List, inventory *Inventory, app *tview.Application) {
+func setHostListSelectedFunc(list *tview.List, hosts []Host, app *tview.Application) {
 	list.SetSelectedFunc(func(index int, mainText, secondaryText string, shortcut rune) {
-		host := inventory.Hosts[index]
+		host := hosts[index]
 
 		if host.Username == "" {
 			fmt.Println("Error: Host username is missing in the inventory.")
 			os.Exit(1)
 		}
 
-		// Close the TUI application
 		app.Stop()
 
-		// Launch the SSH connection in the default terminal using sshpass
 		cmd := exec.Command("sshpass", "-p", host.Password, "ssh", "-o", "StrictHostKeyChecking no", host.Username+"@"+host.Hostname)
 		cmd.Stdout = os.Stdout
 		cmd.Stdin = os.Stdin
@@ -144,33 +141,4 @@ func setHostListSelectedFunc(list *tview.List, inventory *Inventory, app *tview.
 			os.Exit(1)
 		}
 	})
-}
-
-func loadInventoryByIndex(index int) (*Inventory, error) {
-	// Define a map to store inventory paths
-	inventoryPaths := map[int]string{
-		1: os.Getenv("SSHMANAGER_INVENTORY" + strconv.Itoa(index)),
-		2: os.Getenv("SSHMANAGER_INVENTORY" + strconv.Itoa(index)),
-	}
-
-	// Determine the inventory path based on the index
-	inventoryPath, found := inventoryPaths[index]
-	if !found {
-		return nil, fmt.Errorf("Invalid index")
-	}
-
-	if inventoryPath == "" {
-		usr, err := user.Current()
-		if err != nil {
-			return nil, err
-		}
-		inventoryPath = usr.HomeDir + "/inventory" + strconv.Itoa(index) + ".json"
-	}
-
-	inventory, err := loadInventory(inventoryPath)
-	if err != nil {
-		return nil, err
-	}
-
-	return inventory, nil
 }
