74a2886118e8f12f3025b396ffdfa0b7d2357d91
prot0s34 <nikprotosmas@yandex.ru>
2024-01-19 01:20:45 +0300

    attempting to minimize binary size

--- ssm/kube.go
+++ ssm/kube.go
@@ -6,17 +6,17 @@ import (
 	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func initKubernetesClient(kubeconfigPath string) (*kubernetes.Clientset, error) {
+func initKubernetesClient(kubeconfigPath string) (*corev1.CoreV1Client, error) {
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 	if err != nil {
 		return nil, err
 	}
 
-	clientset, err := kubernetes.NewForConfig(config)
+	clientset, err := corev1.NewForConfig(config)
 	if err != nil {
 		return nil, err
 	}
@@ -24,8 +24,8 @@ func initKubernetesClient(kubeconfigPath string) (*kubernetes.Clientset, error) 
 	return clientset, nil
 }
 
-func findPodByKeyword(clientset *kubernetes.Clientset, namespace, keyword string) (string, error) {
-	pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+func findPodByKeyword(client *corev1.CoreV1Client, namespace, keyword string) (string, error) {
+	pods, err := client.Pods(namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		return "", err
 	}
